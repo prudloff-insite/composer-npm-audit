fc857198f5c6e55ed721f5e3b88af617a9973db9
Pierre Rudloff <prudloff@insite.coop>
2019-12-09 15:46:54 +0100

    Get everything in one request

--- src/NpmAuditCommand.php
+++ src/NpmAuditCommand.php
@@ -5,18 +5,13 @@ namespace ComposerNpmAudit;
 use Composer\Command\BaseCommand;
 use Fxp\Composer\AssetPlugin\Converter\NpmPackageUtil;
 use GuzzleHttp\Client;
-use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\RequestOptions;
 use PackageVersions\Versions;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 class NpmAuditCommand extends BaseCommand {
-
-  /**
-   * @var \GuzzleHttp\Client
-   */
-  private $client;
 
   protected function configure() {
     $this->setName('npm-audit');
@@ -29,53 +24,62 @@ class NpmAuditCommand extends BaseCommand {
     $client = new Client();
     $io = new SymfonyStyle($input, $output);
 
+    $requires = [];
+    $dependencies = [];
     foreach (Versions::VERSIONS as $package => $version) {
       $packageInfo = explode('/', $package);
       $versionInfo = explode('@', $version);
       if ($packageInfo[0] == 'npm-asset') {
         $name = NpmPackageUtil::revertName($packageInfo[1]);
-
-        try {
-          // todo Utiliser une requête POST sur http://registry.npmjs.org/-/npm/v1/security/audits.
-          $response = $client->get('http://registry.npmjs.org/-/npm/v1/security/audits/' . $name . '/' . $versionInfo[0]);
-
-          $data = json_decode($response->getBody()->getContents());
-          $io->section($name . ' ' . $versionInfo[0]);
-          if (empty((array) $data->advisories)) {
-            $io->success('No known vulnerability.');
-          }
-          else {
-            $table = [];
-            foreach ($data->advisories as $advisory) {
-              $table[] = [
-                $advisory->severity,
-                $advisory->title,
-                $advisory->module_name,
-                $advisory->vulnerable_versions,
-                $advisory->recommendation,
-                $advisory->url,
-              ];
-            }
-
-            $io->table(
-              [
-                'Severity',
-                'Title',
-                'Dependency',
-                'Vulnerable versions',
-                'Recommendation',
-                'URL',
-              ],
-              $table
-            );
-          }
-        } catch (BadResponseException $e) {
-          $io->error(
-            'Could not get the vulnerabilites for ' . $name . ' ' . $versionInfo[0] . ': ' . PHP_EOL .
-            $e->getMessage()
-          );
-        }
+        $requires[$name] = $versionInfo[0];
+        $dependencies[$name] = [
+          'version' => $versionInfo[0],
+        ];
       }
+    }
+
+    $response = $client->post(
+      'http://registry.npmjs.org/-/npm/v1/security/audits',
+      [
+        RequestOptions::BODY => json_encode([
+          'dependencies' => $dependencies,
+          'requires' => $requires,
+        ]),
+      ]
+    );
+    $results = json_decode($response->getBody()->getContents());
+
+    if (empty((array) $results->advisories)) {
+      $io->success('No known vulnerability.');
+
+      return 0;
+    }
+    else {
+      $table = [];
+      foreach ($results->advisories as $advisory) {
+        $table[] = [
+          $advisory->severity,
+          $advisory->title,
+          $advisory->module_name,
+          $advisory->vulnerable_versions,
+          $advisory->recommendation,
+          $advisory->url,
+        ];
+      }
+
+      $io->table(
+        [
+          'Severity',
+          'Title',
+          'Dependency',
+          'Vulnerable versions',
+          'Recommendation',
+          'URL',
+        ],
+        $table
+      );
+
+      return 1;
     }
   }
 }
